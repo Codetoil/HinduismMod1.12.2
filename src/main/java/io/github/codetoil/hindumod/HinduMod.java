9280c88f442a8465f27f4f0e9f499036911f45d9
Codetoil <ianthisawesomee@gmail.com>
2019-10-30 16:13:31 -0400

    Update Everything

--- src/main/java/io/github/codetoil/hindumod/HinduMod.java
+++ src/main/java/io/github/codetoil/hindumod/HinduMod.java
@@ -1,18 +1,16 @@
-package com.example.examplemod;
+package io.github.codetoil.hindumod;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+@Mod(modid = HinduMod.MODID, name = HinduMod.NAME, version = HinduMod.VERSION)
+public class HinduMod {
+    public static final String MODID = "hindumod";
+    public static final String NAME = "HinduMod";
+    public static final String VERSION = "0.0.0.0";
 
     private static Logger logger;
 
@@ -25,7 +23,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 }
